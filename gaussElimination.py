006687065e5ba6ec787c74a73c16f8c0441916b6
Yossi Elgazar <yossiel4@ac.sce.ac.il>
2022-03-21 15:00:27 +0200

    yossi + solal commit

--- gaussElimination.py
+++ gaussElimination.py
@@ -1,3 +1,30 @@
+def checkPivotMax(matrix, elmatlist):
+    for row in range(len(matrix)):
+        max = matrix[row][row]
+        index = row
+        indexmax = index
+        for index in range(row, len(matrix)):
+            if max < matrix[index][row]:
+                max = matrix[index][row]
+                indexmax = index
+        if indexmax != index:
+            matrix = switchRows(matrix, index, indexmax, elmatlist)
+    return matrix
+
+
+def zeroUnderPivot(matrix, elmatlist):
+    for row in range(len(matrix)):
+        pivot = matrix[row][row]
+        for col in range(row + 1, len(matrix)):
+            if matrix[col][row] != 0:
+                resetnum = (matrix[col][row] / pivot) * -1
+                elmat = createElMat(matrix)
+                elmat[col][row] = resetnum
+                elmatlist.append(elmat)
+                matrix = matrixMul(elmat, matrix)
+    return matrix
+
+
 def buildZeroMatrix(matrix, numOfPops):
     temp = eval(repr(matrix))
     zeroMatrix = []
@@ -11,7 +38,7 @@ def buildZeroMatrix(matrix, numOfPops):
     return zeroMatrix
 
 
-def MatrixMul(mat1, mat2):
+def matrixMul(mat1, mat2):
     newmat = eval(repr(mat1))
     newmat = buildZeroMatrix(newmat, 0)
     for i in range(len(newmat)):
@@ -21,24 +48,43 @@ def MatrixMul(mat1, mat2):
     return newmat
 
 
+def createElMat(matrix):
+    newmat = buildZeroMatrix(matrix, 0)
+    for i in range(0, len(matrix)):
+        newmat[i][i] = 1
+    return newmat
+
+
+def makePivotOne(matrix, elmatlist):
+    for row in range(len(matrix)):
+        if matrix[row][row] != 1:
+            elmat = createElMat(matrix)
+            elmat[row][row] = pow(matrix[row][row], -1)
+            elmatlist.append(elmat)
+            matrix = matrixMul(elmat, matrix)
+    return matrix
+
+
+
+def switchRows(mat, row1, row2, elmatlist):
+    newmat = buildZeroMatrix(mat, 0)
+    for i in range(0, len(mat)):
+        if i == row1:
+            newmat[i][row2] = 1
+        elif i == row2:
+            newmat[i][row1] = 1
+        else:
+            newmat[i][i] = 1
+    elmatlist.append(newmat)
+    return matrixMul(newmat, mat)
+
+
 def checkDiagonal(elmatlist, mat):
     def findValidRow(mat, row):
         for i in range(row + 1, len(mat)):
             if mat[i][row] != 0:
                 return i
         return -1
-
-    def switchRows(mat, row1, row2):
-        newmat = buildZeroMatrix(mat, 1)
-        for i in range(0, len(mat)):
-            if i == row1:
-                newmat[i][row2] = 1
-            elif i == row2:
-                newmat[i][row1] = 1
-            else:
-                newmat[i][i] = 1
-        elmatlist.append(newmat)
-        return MatrixMul(newmat, mat)
 
     for row in range(len(mat)):
         if mat[row][row] == 0:
@@ -48,7 +94,7 @@ def checkDiagonal(elmatlist, mat):
                     if mat[i][row] != 0 and mat[row][i] != 0:
                         rowNum = i
                         break
-            mat = switchRows(mat, row, rowNum)
+            mat = switchRows(mat, row, rowNum, elmatlist)
     return mat
 
 
@@ -65,6 +111,3 @@ def print_matrix(matrix):
             rowString += f'{str(element)} '
         print(rowString)
     print('')
-
-
-print_matrix(MatrixMul([[1, 2], [2, 3]], [[3, 4, 7], [4, 5, 8]]))
